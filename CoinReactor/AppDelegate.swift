91322ae75b13244025c6d10bb84e6756f5a49be9
Roman <romazan.26@gmail.com>
2025-11-26 12:17:27 +0300

    add load

--- CoinReactor/AppDelegate.swift
+++ CoinReactor/AppDelegate.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import AppsFlyerLib
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var restrictRotation: UIInterfaceOrientationMask = .all
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // AppsFlyer Init
+        AppsFlyerLib.shared().appsFlyerDevKey = "H6BZvpvLEyndgeSBs36Yu"
+        AppsFlyerLib.shared().appleAppID = "6755430181"
+        AppsFlyerLib.shared().delegate = self
+        AppsFlyerLib.shared().isDebug = false
+        AppsFlyerLib.shared().disableAdvertisingIdentifier = true
+        AppsFlyerLib.shared().start()
+
+        //OneSignal
+        OneSignalService.shared.requestPermissionAndInitialize()
+        
         return true
     }
 
